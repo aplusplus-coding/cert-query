776db0d28b361631187ab8665c33db4279e108af
hsuan <hsuan@hsuan.app>
2022-07-16 20:37:26 +0800

    Add more information

--- src/views/Query.vue
+++ src/views/Query.vue
@@ -28,25 +28,25 @@ try {
 		<div
 			class="px-4 py-6 mt-10 sm:px-0 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
 		>
-			<div class="text-2xl" v-if="isValid">✅ 驗證成功</div>
-			<div class="text-2xl" v-else>❌ 驗證失敗</div>
+			<div class="text-3xl" v-if="isValid">✅ 驗證成功 <span class="font-bold">{{ payload.jti }}</span></div>
+			<div class="text-3xl" v-else>❌ 驗證失敗</div>
 			<p
 				class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
 			>
-				{{ payload.iss }}
+				簽發者： <span class="font-bold">{{ payload.iss }}</span>({{ new Date(payload.iat).toLocaleString() }})
 			</p>
 			<p
 				class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
 			>
-				{{ payload.id }}
+				擁有者： <span class="font-bold">{{ payload.owner }}</span>
 			</p>
 			<p
-				class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				class="mt-3 text-lg text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
 			>
-				擁有者：{{ payload.owner }}
+				<span class="font-bold">{{ payload.title }}</span>
 			</p>
 			<p
-				class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				class="mt-3 text-base text-gray-800  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
 			>
 				{{ payload.body }}
 			</p>
