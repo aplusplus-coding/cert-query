859c8e47d53b99123f94de348e28317a971b6c7e
hsuan <hsuan@hsuan.app>
2022-07-18 16:39:45 +0800

    New Query

--- src/views/Query.vue
+++ src/views/Query.vue
@@ -2,6 +2,7 @@
 import * as jose from 'jose'
 import {useRoute} from 'vue-router'
 import {CheckCircleIcon} from '@heroicons/vue/outline';
+
 const router = useRoute()
 let isValid = true, payload, protectedHeader;
 const pubkey = `-----BEGIN PUBLIC KEY-----
@@ -28,28 +29,33 @@ try {
 		<div
 			class="px-4 py-6 mt-10 sm:px-0 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
 		>
-			<div class="text-3xl" v-if="isValid">✅ 驗證成功 <span class="font-bold">{{ payload.jti }}</span></div>
-			<div class="text-3xl" v-else>❌ 驗證失敗</div>
-			<p
-				class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-			>
-				簽發者： <span class="font-bold">{{ payload.iss }}</span>({{ new Date(payload.iat).toLocaleString() }})
-			</p>
-			<p
-				class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-			>
-				擁有者： <span class="font-bold">{{ payload.owner }}</span>
-			</p>
-			<p
-				class="mt-3 text-lg text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-			>
-				<span class="font-bold">{{ payload.title }}</span>
-			</p>
-			<p
-				class="mt-3 text-base text-gray-800  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-			>
-				{{ payload.body }}
-			</p>
+			<template v-if="isValid">
+				<div class="text-3xl">✅ 驗證成功 <span class="font-bold">{{ payload.jti }}</span></div>
+			</template>
+			<template v-else>
+
+				<div class="text-3xl">❌ 驗證失敗</div>
+				<p
+					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					簽發者： <span class="font-bold">{{ payload.iss }}</span>({{ new Date(payload.iat).toLocaleString() }})
+				</p>
+				<p
+					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					擁有者： <span class="font-bold">{{ payload.owner }}</span>
+				</p>
+				<p
+					class="mt-3 text-lg text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					<span class="font-bold">{{ payload.title }}</span>
+				</p>
+				<p
+					class="mt-3 text-base text-gray-800  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					{{ payload.body }}
+				</p>
+			</template>
 		</div>
 	</main>
 </template>
