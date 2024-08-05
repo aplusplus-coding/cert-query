39acae1a6aeb7642ee9dbe92e295d2b5c0e13673
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-08-05 22:11:08 +0800

    Add SCanner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,33 @@
-<script setup></script>
+<script setup>
+import {ref} from "vue";
+import {StreamBarcodeReader} from "vue-barcode-reader";
+
+const decodedText = ref("");
+
+const onDecode = (text) => {
+    decodedText.value = text;
+    alert(text);
+};
+
+const onLoad = (code) => {
+    console.log(code);
+    alert(code);
+}
+</script>
 <template>
-	<main>
-		<div
-			class="px-4 py-6 mt-10  sm:px-0 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
-		>
-			<img class="" src="/img/logo-rect.png" alt="Logo"/>
-			<div class="sm:text-center lg:text-left">
-				<p class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-					所有A++程式設計或相關合作發證組織所簽發之數位證明可於此查詢真實性。
-				</p>
-			</div>
-		</div>
-	</main>
+    <main>
+        <div
+            class="px-4 py-6 mt-10  sm:px-0 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+        >
+            <img class="" src="/img/logo-rect.png" alt="Logo"/>
+            <div class="sm:text-center lg:text-left">
+                <p class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                    所有A++程式設計或相關合作發證組織所簽發之數位證明可於此查詢真實性。
+                </p>
+            </div>
+
+            <StreamBarcodeReader @decode="onDecode" @loaded="onLoad"></StreamBarcodeReader>
+            <h2>{{ decodedText }}</h2>
+        </div>
+    </main>
 </template>
