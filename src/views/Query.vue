d23b6a87e556d468ff627359ca9487afaf87dd4e
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-08-05 22:02:49 +0800

    Add Fields

--- src/views/Query.vue
+++ src/views/Query.vue
@@ -34,12 +34,32 @@ try {
 				<p
 					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
 				>
-					簽發者： <span class="font-bold">{{ payload.iss }}</span>({{ new Date(payload.iat).toLocaleString() }})
+					證書類別： <span class="font-bold">{{ payload.sub }}</span>
+				</p>
+                <p
+					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					簽發者： <span class="font-bold">{{ payload.iss }}</span>
+				</p>
+                <p
+					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					簽發時間： {{ new Date(payload.iat).toLocaleString() }}
+				</p>
+                <p
+					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					生效日期： {{ new Date(payload.nbf).toLocaleString() }}
+				</p>
+                <p
+					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+				>
+					有效日期： {{ new Date(payload.exp).toLocaleString() }}
 				</p>
 				<p
 					class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
 				>
-					擁有者： <span class="font-bold">{{ payload.owner }}</span>
+					擁有者： <span class="font-bold">{{ payload.owner }} {{ payload.aud }}</span>
 				</p>
 				<p
 					class="mt-3 text-lg text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
