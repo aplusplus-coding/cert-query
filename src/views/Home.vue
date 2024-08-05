5287d4f36661ee2a779c2f0cf67d595bfb49f13a
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-08-05 22:53:58 +0800

    Add SCanner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,27 @@ import {ref} from "vue";
 import {StreamBarcodeReader} from "vue-barcode-reader";
 
 const decodedText = ref("");
-
+const show = ref(false);
 const onDecode = (text) => {
     decodedText.value = text;
-    alert(text);
+    if (text) {
+        if (text.startsWith("http")) {
+            window.open(text, "_blank");
+        } else {
+            const type = text[0];
+            const lang_type = {
+                "1": "獎狀"
+            }
+            alert("您掃描的證書類型為：" + lang_type[type]);
+            const code_letter = String.fromCharCode(String(text.slice(1, 4))) + String(text.slice(4, 7));
+            const code_number = text.slice(7, 13);
+            alert("您掃描的證書號為：" + code_letter + code_number);
+        }
+    }
 };
 
 const onLoad = (code) => {
-    console.log(code);
-    alert(code);
+    alert("請掃描條碼或QR Code，掃描條碼時請將條碼放置於框框中，左右些微突出");
 }
 </script>
 <template>
@@ -22,12 +34,20 @@ const onLoad = (code) => {
             <img class="" src="/img/logo-rect.png" alt="Logo"/>
             <div class="sm:text-center lg:text-left">
                 <p class="mt-3 text-base text-gray-500  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                    所有A++程式設計或相關合作發證組織所簽發之數位證明可於此查詢真實性。
+                    所有本營隊或相關合作發證組織所簽發之數位證明可於此查詢真實性。
                 </p>
             </div>
 
-            <StreamBarcodeReader @decode="onDecode" @loaded="onLoad"></StreamBarcodeReader>
-            <h2>{{ decodedText }}</h2>
+
+            <button @click="show = !show"
+                    class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                掃描條碼
+            </button>
+
+            <div v-if="show">
+                <StreamBarcodeReader @decode="onDecode" @loaded="onLoad"></StreamBarcodeReader>
+                <h2>{{ decodedText }}</h2>
+            </div>
         </div>
     </main>
 </template>
